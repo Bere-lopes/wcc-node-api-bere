d01ee0c0f9d4072d4c39879911f81c8a88d8810d
Berenice <berenicelopesmaria@gmal.com>
2021-11-17 02:21:18 -0300

    API primeiros

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+app.use(express.json());
+
 app.get("/", function(req, res)  {
         res.send("Minha primeira requisição");
 });
@@ -11,7 +13,28 @@ app.get("/segunda-req", function(req, res) {
 });
 
 app.get("/com-parametros", function(req, res)  {
+    if(req.query.nome === "Berenice")  {
+        res.send("Berenice chamou requisição"); 
+    }
     res.send("Com parâmetro funciona?  Sabadou " + req.query.nome);
+});
+
+app.post("/meu-primeiro-post", function (req, res) {
+    console.log(req.body);
+    res.send("Meu post funciona");
+
+});
+
+app.put("/meu-primeiro-put/:id", function(req, res)  {
+    console.log(req.body, req.params.id);
+    res.send("Meu put funciona");
+
+});
+
+app.delete("/meu-primeiro-delete/:id", function(req, res)  {
+    console.log(req.params.id);
+    res.send("Meu delete funciona" + req.params.id);
+
 });
 
 app.listen(port, function() {
